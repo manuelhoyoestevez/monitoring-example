9db4110dd9aa4fb96fa81337a10a9836631fcf6b
Manuel Hoyo Estévez <manuel.hoyo@inetum.world>
2021-07-14 15:04:17 +0200

    Two telegraf working through rabbitmq

---
+++ js-scripts/receive.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const exchangeName = 'influx';
+const exchangeType = 'direct';
+const routingKey = 'telegraf';
+
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://rabbitmq:rabbitmq@localhost:5672/', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        channel.assertExchange(exchangeName, exchangeType, { durable: true });
+
+        channel.assertQueue('telegraf', { exclusive: true }, function(error2, q) {
+            if (error2) {
+                throw error2;
+            }
+
+            console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+            channel.bindQueue(q.queue, exchangeName, routingKey);
+
+            channel.consume(q.queue, function(msg) {
+                console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+            }, { noAck: true });
+        });
+    });
+});
