7c63b381e95b856201fbaab39d315012fce1002f
Manuel Hoyo Estévez <manuel.hoyo@inetum.world>
2021-07-14 20:51:35 +0200

    Updated node scripts

--- js-scripts/send.js
+++ js-scripts/send.js
@@ -1,26 +1,28 @@
 #!/usr/bin/env node
 
+const exchangeName = 'influx';
+const exchangeType = 'direct';
+const routingKey = 'telegraf';
+
 const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(error0, connection) {
+amqp.connect('amqp://rabbitmq:rabbitmq@localhost:5672/', function(error0, connection) {
     if (error0) {
         throw error0;
     }
+
     connection.createChannel(function(error1, channel) {
         if (error1) {
             throw error1;
         }
 
-        const queue = 'hello';
         const msg = 'Hello World!';
 
-        channel.assertQueue(queue, {
-            durable: false
-        });
-        channel.sendToQueue(queue, Buffer.from(msg));
-
-        console.log(" [x] Sent %s", msg);
+        channel.assertExchange(exchangeName, exchangeType, { durable: true });
+        channel.publish(exchangeName, routingKey, Buffer.from(msg));
+        console.log(" [x] Sent %s: '%s'", routingKey, msg);
     });
+
     setTimeout(function() {
         connection.close();
         process.exit(0);
